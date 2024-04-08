4504d06d1711114ad9c94bd16e96c781400f2a35
kharlamcik <kkkharlamcik@vk.com>
2024-04-08 22:55:31 +0700

    Кароче пол хома

--- lib/feature/home/widget/CustomContainerWidget.dart
+++ lib/feature/home/widget/CustomContainerWidget.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CustomContainerWidget extends StatelessWidget {
-  const CustomContainerWidget({super.key, required this.text, required this.exp, required this.asset1, });
+class CustomContainerWidget extends StatefulWidget {
+  const CustomContainerWidget({
+    Key? key,
+    required this.text,
+    required this.exp,
+    required this.asset1,
+  }) : super(key: key);
 
   final String text;
   final int exp;
   final String asset1;
 
+  @override
+  _CustomContainerWidgetState createState() => _CustomContainerWidgetState();
+}
+
+class _CustomContainerWidgetState extends State<CustomContainerWidget> {
+  bool isButtonPressed = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +26,7 @@ class CustomContainerWidget extends StatelessWidget {
       width: 374,
       height: 80,
       decoration: ShapeDecoration(
-        color: Color(0xFF8E97FD),
+        color: isButtonPressed ? Colors.white : Color(0xFF8E97FD),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
         ),
@@ -25,57 +36,64 @@ class CustomContainerWidget extends StatelessWidget {
             blurRadius: 4,
             offset: Offset(0, 4),
             spreadRadius: 0,
-          )
+          ),
         ],
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              asset1,
-              height: 58,
-              width: 58,
-              color: Colors.white,
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                text,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w500,
-                  height: 0,
-                ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                widget.asset1,
+                height: 58,
+                width: 58,
+                color: isButtonPressed ? Colors.black : Colors.white,
               ),
-              Text(
-                '$exp exp',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w300,
-                  height: 0,
-                ),
-              ),
-            ],
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'Assets/icons/add_file.svg',
-              height: 58,
-              width: 58,
-              color: Colors.white,
             ),
-          ),
-        ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.text,
+                  style: TextStyle(
+                    color: isButtonPressed ? Colors.black : Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+                Text(
+                  '${widget.exp} exp',
+                  style: TextStyle(
+                    color: isButtonPressed ? Colors.black : Colors.white,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w300,
+                    height: 0,
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isButtonPressed = !isButtonPressed;
+                });
+              },
+              icon: SvgPicture.asset(
+                isButtonPressed ? 'Assets/icons/check.svg' : 'Assets/icons/add_file.svg',
+                height: 24,
+                width: 24,
+                color:  isButtonPressed ? Colors.black : Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
